c35f4242f7ae757b0cde16f6c1820074946b5e19
LuisZavala30 <65318000+LuisZavala30@users.noreply.github.com>
2020-05-21 23:10:55 -0600

    Add files via upload

---
+++ Hogar.h
@@ -0,0 +1,133 @@
+// Proyecto Domotica
+// Luis Angel Zavala Robles
+// A01706481
+
+
+/* Esta clase define objetos de tipo Dispositivo y realiza diversas operaciones
+ generales sobre ellos*/
+
+#ifndef HOGAR_H_
+#define HOGAR_H_
+
+#include <iostream>
+
+#include "Dispositivo.h" //clase de mi proyecto
+
+
+using namespace std;
+
+
+class Hogar {
+//Variables privadas de clase Hogar
+    private:
+        Dispositivo disp[100];
+        string nombre;
+        int num;
+
+//Metodos publicos y constructor de la clase
+    public:
+
+        Hogar(string _nombre){
+            nombre = _nombre;
+            num = 0;
+        }
+
+        void mostrar_dispositivos();
+        void encender_focos();
+        void apagar_focos();
+        void calc_gasto_total();
+        void agregar_foco(int _id, int _potencia, string _ubicacion);
+        void agregar_aire (int _id, int _temperatura, string _ubicacion);
+        void agregar_ventilador(int _id, string _velocidad, string _ubicacion);
+
+
+};
+
+
+void Hogar :: mostrar_dispositivos(){
+
+    for (int i=0 ; i<num ; i++)
+
+    {
+        cout <<"\nDispositivo: " << disp[i].get_tipo() << "\nId: "<<disp[i].get_id() <<"\nStatus: "<<disp[i].get_status()<<
+        "\nUbicacion: "<<disp[i].get_ubicacion()<< endl;
+    }
+}
+
+void Hogar :: encender_focos(){
+
+    stringstream mensaje;
+    mensaje <<"ALL-ON-"<<nombre;
+
+    /*La variable mensaje se enviara al hardware mediante un protocolo de transmision por definir.
+        En el hardware se va decodificar y se realizara la accion correspondiente*/
+
+    cout <<"Encendidos..."<<endl;
+
+    for (int i=0 ; i<num ; i++)
+    {
+        if(disp[i].get_tipo() == "Foco")
+        {
+            disp[i].set_status(true);
+        }
+    }
+
+}
+
+void Hogar :: apagar_focos(){
+
+    stringstream mensaje;
+    mensaje <<"ALL-OFF-"<<nombre;
+
+    /*La variable mensaje se enviara al hardware mediante un protocolo de transmision por definir.
+        En el hardware se va decodificar y se realizara la accion correspondiente*/
+
+    cout <<"Apagados..."<<endl;
+
+    for (int i=0 ; i<num ; i++)
+    {
+        if(disp[i].get_tipo() == "Foco")
+        {
+            disp[i].set_status(false);
+        }
+    }
+}
+
+void Hogar :: calc_gasto_total() {
+    float gasto_total;
+
+    for (int i=0 ; i<num ; i++)
+    {
+        if(disp[i].get_tipo()== "Foco")
+        {
+           //gasto_total = gasto_total + disp[i].calcular_gasto();
+        }
+    }
+
+    cout << "El gasto total al dia de hoy es: "<< gasto_total;
+}
+
+
+void Hogar :: agregar_foco (int _id, int _potencia, string _ubicacion){
+
+    disp[num] =  Foco("Foco", _id, _ubicacion, _potencia);
+    cout<<"\nFoco Agregado"<<endl;
+    num++;
+}
+
+void Hogar :: agregar_aire (int _id, int _temperatura, string _ubicacion){
+
+    disp[num] = Aire("Aire", _id, _ubicacion, _temperatura);
+    cout<<"\nAire Agregado"<<endl;
+    num++;
+}
+
+void Hogar :: agregar_ventilador(int _id, string _velocidad, string _ubicacion){
+
+    disp[num] = Ventilador("Ventilador", _id , _ubicacion, _velocidad);
+    cout<<"\nVentilador Agregado"<<endl;
+    num++;
+
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,40 @@
+//Proyecto Domotica
+// Luis Angel Zavala Robles
+// A01706481
+
+/* Proyecto para realizar pruebas de ciertos dispositivos de domotica con base en
+    los requerimientos del usuario. Es parte del software de un proyecto integral*/
+
+#include <iostream>
+#include "Dispositivo.h" // Clase de mi proyecto
+#include "Hogar.h" // Clase de mi proyecto
+
+
+
+
+int main(){
+
+    cout<<"PRUEBA DE FUNCIONAMIENTO HASTA EL MOMENTO CON LA IMPLEMENTACION DE HERENCIA, SOBREESCRITURA Y SOBRECARGA\n";
+    cout<<"LA VERSION FINAL CONTARA CON INTERFAZ GRAFICA PARA UN FUNCIONAMIENTO MAS DINAMICO"<<endl;
+
+    Hogar Casa1("Casa1");
+    cout<<"1-Test agregar_foco: ";
+    Casa1.agregar_foco(1,70,"Recamara");
+
+    cout<<"\n2-Test agregar_aire: ";
+    Casa1.agregar_aire(2,24,"Sala");
+
+    cout<<"\n3-Test agregar_ventilador: ";
+    Casa1.agregar_ventilador(3,"Slow","Jardin");
+
+    cout<<"\n4-Test apagar_focos: ";
+    Casa1.apagar_focos();
+
+    cout<<"\n5-Test encender_focos: ";
+    Casa1.encender_focos();
+
+    cout<<"\n6-Test mostrar_dispositivos: \n";
+    Casa1.mostrar_dispositivos();
+
+
+}
