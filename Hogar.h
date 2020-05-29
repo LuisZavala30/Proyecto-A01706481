dd4e5d5b910814ff62632c6d3784207ac0f34bd2
LuisZavala30 <65318000+LuisZavala30@users.noreply.github.com>
2020-05-28 21:45:49 -0600

    Upload Class

--- Hogar.h
+++ Hogar.h
@@ -20,7 +20,7 @@ using namespace std;
 class Hogar {
 //Variables privadas de clase Hogar
     private:
-        Dispositivo disp[100];
+        Dispositivo *disp[100];  //Apuntador para  usar polimorfismo
         string nombre;
         int num;
 
@@ -33,12 +33,16 @@ class Hogar {
         }
 
         void mostrar_dispositivos();
-        void encender_focos();
-        void apagar_focos();
+        void encender_todos();
+        void apagar_todos();
         void calc_gasto_total();
         void agregar_foco(int _id, int _potencia, string _ubicacion);
         void agregar_aire (int _id, int _temperatura, string _ubicacion);
         void agregar_ventilador(int _id, string _velocidad, string _ubicacion);
+
+        int get_num(){return num;}
+        Dispositivo** get_disp(){return disp;}
+
 
 
 };
@@ -49,12 +53,28 @@ void Hogar :: mostrar_dispositivos(){
     for (int i=0 ; i<num ; i++)
 
     {
-        cout <<"\nDispositivo: " << disp[i].get_tipo() << "\nId: "<<disp[i].get_id() <<"\nStatus: "<<disp[i].get_status()<<
-        "\nUbicacion: "<<disp[i].get_ubicacion()<< endl;
+        if(disp[i]->get_tipo() == "Foco")
+        {
+            cout <<"\nDispositivo: " << disp[i]->get_tipo() << "\nId: "<<disp[i]->get_id() <<"\nStatus: "<<disp[i]->get_status()<<
+            "\nUbicacion: "<<disp[i]->get_ubicacion()<< endl;
+        }
+
+        else if(disp[i]->get_tipo() == "Aire")
+        {
+            cout <<"\nDispositivo: " << disp[i]->get_tipo() << "\nId: "<<disp[i]->get_id() <<"\nStatus: "<<disp[i]->get_status()<<
+            "\nUbicacion: "<<disp[i]->get_ubicacion()<<"\nTemperatura: "<< dynamic_cast<Aire*>(disp[i])->get_temp()<<endl;
+        }
+        else
+        {
+            cout <<"\nDispositivo: " << disp[i]->get_tipo() << "\nId: "<<disp[i]->get_id() <<"\nStatus: "<<disp[i]->get_status()<<
+            "\nUbicacion: "<<disp[i]->get_ubicacion()<<"\nTemperatura: "<< dynamic_cast<Ventilador*>(disp[i])->get_vel()<<endl;
+
+        }
+
     }
 }
 
-void Hogar :: encender_focos(){
+void Hogar :: encender_todos(){
 
     stringstream mensaje;
     mensaje <<"ALL-ON-"<<nombre;
@@ -62,19 +82,18 @@ void Hogar :: encender_focos(){
     /*La variable mensaje se enviara al hardware mediante un protocolo de transmision por definir.
         En el hardware se va decodificar y se realizara la accion correspondiente*/
 
-    cout <<"Encendidos..."<<endl;
+    cout <<"Todos Encendidos..."<<endl;
 
     for (int i=0 ; i<num ; i++)
     {
-        if(disp[i].get_tipo() == "Foco")
-        {
-            disp[i].set_status(true);
-        }
+
+        disp[i]->set_status(true);
+
     }
 
 }
 
-void Hogar :: apagar_focos(){
+void Hogar :: apagar_todos(){
 
     stringstream mensaje;
     mensaje <<"ALL-OFF-"<<nombre;
@@ -82,14 +101,13 @@ void Hogar :: apagar_focos(){
     /*La variable mensaje se enviara al hardware mediante un protocolo de transmision por definir.
         En el hardware se va decodificar y se realizara la accion correspondiente*/
 
-    cout <<"Apagados..."<<endl;
+    cout <<"Todos Apagados..."<<endl;
 
     for (int i=0 ; i<num ; i++)
     {
-        if(disp[i].get_tipo() == "Foco")
-        {
-            disp[i].set_status(false);
-        }
+
+        disp[i]->set_status(false);
+
     }
 }
 
@@ -98,9 +116,9 @@ void Hogar :: calc_gasto_total() {
 
     for (int i=0 ; i<num ; i++)
     {
-        if(disp[i].get_tipo()== "Foco")
+        if(disp[i]->get_tipo()== "Foco")
         {
-           //gasto_total = gasto_total + disp[i].calcular_gasto();
+           gasto_total = gasto_total + dynamic_cast<Foco*>(disp[i])->calcular_gasto();
         }
     }
 
@@ -110,21 +128,21 @@ void Hogar :: calc_gasto_total() {
 
 void Hogar :: agregar_foco (int _id, int _potencia, string _ubicacion){
 
-    disp[num] =  Foco("Foco", _id, _ubicacion, _potencia);
+    disp[num] =  new Foco("Foco", _id, _ubicacion, _potencia);  //Objeto creado en tiempo de ejecucion (Polimorfismo)
     cout<<"\nFoco Agregado"<<endl;
     num++;
 }
 
 void Hogar :: agregar_aire (int _id, int _temperatura, string _ubicacion){
 
-    disp[num] = Aire("Aire", _id, _ubicacion, _temperatura);
+    disp[num] = new Aire("Aire", _id, _ubicacion, _temperatura);  //Objeto creado en tiempo de ejecucion (Polimorfismo)
     cout<<"\nAire Agregado"<<endl;
     num++;
 }
 
 void Hogar :: agregar_ventilador(int _id, string _velocidad, string _ubicacion){
 
-    disp[num] = Ventilador("Ventilador", _id , _ubicacion, _velocidad);
+    disp[num] = new Ventilador("Ventilador", _id , _ubicacion, _velocidad); //Objeto creado en tiempo de ejecucion (Polimorfismo)
     cout<<"\nVentilador Agregado"<<endl;
     num++;
 
